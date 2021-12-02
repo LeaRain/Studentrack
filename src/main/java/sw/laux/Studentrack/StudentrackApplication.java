73b6cdc6a028c6c6e671230c26416b5f848e4701
lea <blue.blood@gmx.de>
2021-12-02 19:28:01 +0100

    Add first steps for frontend

--- src/main/java/sw/laux/Studentrack/StudentrackApplication.java
+++ src/main/java/sw/laux/Studentrack/StudentrackApplication.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
-@RestController
 public class StudentrackApplication {
 
 	public static void main(String[] args) {
