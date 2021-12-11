f5b59c5eab00d5ddb6fd0cd05080c8af3ff896ba
lea <blue.blood@gmx.de>
2021-12-11 18:42:00 +0100

    More login basics

--- src/main/java/sw/laux/Studentrack/presentation/controller/LoginController.java
+++ src/main/java/sw/laux/Studentrack/presentation/controller/LoginController.java
@@ -3,7 +3,9 @@ package sw.laux.Studentrack.presentation.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import sw.laux.Studentrack.application.services.UserServiceInternal;
 import sw.laux.Studentrack.persistence.entities.UserImplementation;
 import sw.laux.Studentrack.persistence.entities.UserWebShell;
@@ -19,5 +21,18 @@ public class LoginController {
         model.addAttribute("user", user);
 
         return "login";
+    }
+
+    @RequestMapping(value = "login/check", method = RequestMethod.POST)
+    public String doLogin(Model model,
+                          @ModelAttribute("user") UserImplementation user) {
+
+        return "redirect:/home";
+
+    }
+
+    @RequestMapping(value = "home")
+    public String home(Model model) {
+        return "home";
     }
 }
